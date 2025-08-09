5bacbc0e74e064088bdaa6a2b50565beca33e13a
Yurohime <halimlimantoro21@gmail.com>
2025-08-09 14:49:50 +0700

    Initial Commit

---
+++ DSKSpriteKitVersion/DSKSpriteKitVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSKSpriteKitVersionApp.swift
+//  DSKSpriteKitVersion
+//
+//  Created by Alberto Halim Limantoro on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct DSKSpriteKitVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DSKSpriteKitVersionTests/DSKSpriteKitVersionTests.swift
@@ -0,0 +1,17 @@
+//
+//  DSKSpriteKitVersionTests.swift
+//  DSKSpriteKitVersionTests
+//
+//  Created by Alberto Halim Limantoro on 09/08/25.
+//
+
+import Testing
+@testable import DSKSpriteKitVersion
+
+struct DSKSpriteKitVersionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
